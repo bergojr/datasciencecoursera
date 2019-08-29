bee5b7dc5f1c4df8e593e73799b6ec9fe378b8d4
bergojr <bergojrmg@gmail.com>
2019-08-29 00:52:37 -0300

    envio de rankall

--- ProgrammingAssignment3.R
+++ ProgrammingAssignment3.R
@@ -16,10 +16,63 @@ best <- function(state, outcome){
   # dados disponibilizados um nível acima para facilitar exercício
   
   ## Check that state and outcome are valid
+  
+  #a função validestateoutcome vai receber o estado e o problema (outcome) selecionado
+  # verifica se são válidos ambos e caso positivo retorna uma lista com todos os 
+  #hospitais e problemas do estado.
+  
+  dados.estado.selecionado <- validatestateoutcome(state,outcome)
+  
+  # a função stateoutcomefilter recebe o problema (outcome) e a lista de hospitais 
+  # do estado selecionado, devolvendo uma lista de casos completos (sem NA) dos 
+  # hospitais do estado selecionado
+  
+  dados.filtrados <- stateoutcomefilter(outcome,dados.estado.selecionado)
+  
+  dados.filtrados.ordenados <- dados.filtrados[order(dados.filtrados[outcome],dados.filtrados["Hospital"]),]
+  dados.filtrados.ordenados[1,"Hospital"]  # Saída correta
+  #dados.filtrados.ordenados #saida para teste
+
+}
+
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  # dados disponibilizados um nível acima para facilitar exercício
+  
+  ## Check that state and outcome are valid
+  dados.estado.selecionado <- validatestateoutcome(state,outcome)
+  
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+  dados.filtrados <- stateoutcomefilter(outcome, dados.estado.selecionado)
+  dados.filtrados.ordenados <- dados.filtrados[order(dados.filtrados[outcome],dados.filtrados["Hospital"]),]
+  numlinhas <- nrow(dados.filtrados)
+  
+  rankeamento <- 1:numlinhas
+  dados.rankeados <- cbind(dados.filtrados.ordenados,rankeamento)
+  names(dados.rankeados) <- c("Hospital.name", "Rate","Rank")
+  melhorhospital <- min(dados.rankeados$Rank)
+  piorhospital <- max(dados.rankeados$Rank)
+  
+  if (num == "best"){
+    return(dados.rankeados[1,1])
+  } else if (num == "worst"){
+    return(dados.rankeados[numlinhas,1])
+  } else if (num < melhorhospital || num > piorhospital ){
+    return(NA)
+  } else{
+    return(dados.rankeados[num,1])
+  }
+
+}
+
+
+
+validatestateoutcome <- function(state, outcome){
   estado.selecionado <- dados$State == state
   
   dados.estado.selecionado <- dados[estado.selecionado,]
-  print(dados.estado.selecionado)
+  
   if (!any(estado.selecionado)){
     print("Estado selecionado não existe na base de dados!!!")
     break
@@ -31,13 +84,18 @@ best <- function(state, outcome){
     print("Problema selecionado não é válido!!!")
     break
   }
-  
+  return(dados.estado.selecionado)
+}
 
-  hospital.outcome <- dados.estado.selecionado[,names(dados.estado.selecionado) %in% c("Hospital", outcome)]
+
+
+stateoutcomefilter <- function(outcome, dados.estado.selecionado ){
+  
+  hospital.outcome <- dados.estado.selecionado[,names(dados.estado.selecionado)%in% c("Hospital", outcome)]
+  
   hospital.outcome.validos <- complete.cases(hospital.outcome)
   dados.filtrados<- hospital.outcome[hospital.outcome.validos, c("Hospital", outcome)]
-  dados.filtrados.ordenados <- dados.filtrados[order(dados.filtrados[outcome],dados.filtrados["Hospital"]),]
-  dados.filtrados.ordenados[1,"Hospital"]  # Saída correta
-  #dados.filtrados.ordenados #saida para teste
-
+  
+  return(dados.filtrados)
 }
+  
---
+++ rankall.R
@@ -0,0 +1,53 @@
+# Este projeto é requisito para finalizar a semana 4 do segundo Curso de Data Science
+# R Programming
+outcomes <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+
+dados <- outcomes[, c(2,6,7,11,17,23)]
+dados[,c(4,5,6)] <-lapply(dados[,c(4,5,6)], as.numeric)
+valid.outcome <- c("heartattack", "heartfailure", "pneumonia")
+names(dados) <- c("Hospital","City","State", valid.outcome)
+resultado <- data.frame()
+
+
+
+#dados.selecionados <- dados[dados$State = state]
+
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  
+  ## Check that state and outcome are valid
+  problema.selecionado <- valid.outcome == outcome
+  
+  if(!any(problema.selecionado)){
+    print("Problema selecionado não é válido!!!")
+    break
+  }
+  
+  outcome.selecionado <- cbind(dados[,1:3],dados[outcome])
+  dados.por.estado <- split(outcome.selecionado,outcome.selecionado$State)
+  
+  for (estado in dados.por.estado){
+    estado.ordenado <- estado[order(estado[outcome],estado["Hospital"]),]
+    numlinhas <- nrow(estado)
+    print(numlinhas)
+    if(num == "best"){
+      melhor <- estado.ordenado[1,c("Hospital","State")]
+      resultado <- rbind(resultado, melhor)
+    } else if( num == "worst"){
+      pior <- estado.ordenado[numlinhas,c("Hospital","State")]
+      resultado <- rbind(resultado, pior)
+    }else{
+      selecao <- estado.ordenado[num,c("Hospital","State")]
+      resultado <- rbind(resultado, selecao)
+    }
+  }
+  resultado
+
+    #for(tabela in teste2){print(tabela[,c(4:6)])}
+    
+  ## For each state, find the hospital of the given rank
+  
+  ## Return a data frame with the hospital names and the
+  ## (abbreviated) state name
+}
+
